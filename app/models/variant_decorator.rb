2ebc73e01228c386891b46d118b24f697bee3521
Andrew Hooker <adhlssu07@gmail.com>
2011-06-04 22:27:13 -0500

    making flat percent work again

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -1,4 +1,10 @@
 Variant.class_eval do
+
+def price
+	return read_attribute(:price) if User.current.nil? or User.current.user_group.nil?
+	UserGroupsVariant.where(:user_group_id => User.current.user_group.id, :variant_id => self.id).try(:first).try(:price) || read_attribute(:price)
+end
+
   def price_for_user(user)
     if user && user.user_group
       user.user_group.calculator.compute_item(self)
