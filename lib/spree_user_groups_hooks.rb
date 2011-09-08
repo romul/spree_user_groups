b9aa12215a2bfbd2ea0d2efe807df6e5f86a2bc5
Sebastian Krieger <sebastian-krieger@online.de>
2011-09-08 11:48:38 +0200

    move user groups to its own admin section

--- lib/spree_user_groups_hooks.rb
+++ lib/spree_user_groups_hooks.rb
@@ -1,6 +1,7 @@
 class SpreeUserGroupsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_user_form_roles, 'admin/users/user_group'
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t(:user_groups), admin_user_groups_path, I18n.t(:manage_user_groups)) %>"
+  
+  insert_after :admin_tabs do 
+    '<%= tab :user_groups %>'
   end
 end
