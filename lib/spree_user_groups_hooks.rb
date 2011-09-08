a135d2e48997df09c4ecbb224ff367d4bfefd3ad
Sebastian Krieger <sebastian-krieger@online.de>
2011-09-08 11:03:22 +0200

    changes placement of user groups
    moved from configuration to users menu

--- lib/spree_user_groups_hooks.rb
+++ lib/spree_user_groups_hooks.rb
@@ -1,6 +1,4 @@
 class SpreeUserGroupsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_user_form_roles, 'admin/users/user_group'
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t(:user_groups), admin_user_groups_path, I18n.t(:manage_user_groups)) %>"
   end
 end
