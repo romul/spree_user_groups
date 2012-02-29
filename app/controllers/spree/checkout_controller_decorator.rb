a2e2d06d156d4d47196da8c6ec3544c7ae7bd47a
Christian Varga <christian@peacock.st>
2012-02-29 12:17:52 +1100

    Spree 1.0.0 ready! Apply discount at cart instead of checkout.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,9 +1,15 @@
-Spree::CheckoutController.class_eval do
-  private
-  def before_address
-    # @order.bill_address ||= Address.default
-    # @order.ship_address ||= Address.default
+# Spree::CheckoutController.class_eval do
+#   private
+#   # def before_address
+#   #   # @order.bill_address ||= Address.default
+#   #   # @order.ship_address ||= Address.default
     
-    @order.user.create_adjustment_for(@order) if @order.user
-  end
-end
+#   #   @order.user.create_adjustment_for(@order) if @order.user
+#   # end
+
+#   before_filter :apply_group_adjustment
+
+#   def apply_group_adjustment
+#     @order.user.create_adjustment_for(@order) if @order.user
+#   end
+# end
--- app/models/spree/variant_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::Variant.class_eval do
-  def price_for_user(user)
-    if user && user.user_group
-      user.user_group.calculator.compute_item(self)
-    else
-      price
-    end
-  end
-end
