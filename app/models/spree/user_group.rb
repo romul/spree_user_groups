bf73cdf214cc8b7bf3223ee5d33205e258ce6adf
David Silva <davidslv@thinkorange.pt>
2012-10-24 18:03:44 +0200

    Update app/models/spree/user_group.rb
    rails 3.2.x compatible

--- app/models/spree/user_group.rb
+++ app/models/spree/user_group.rb
@@ -1,8 +1,11 @@
 class Spree::UserGroup < ActiveRecord::Base
   has_many :users, :dependent => :nullify
-  validates :name, :presence => true
   has_many :user_groups_variants
   has_many :variants, :through => :user_groups_variants
+  
+  attr_accessible :name
+  validates :name, :presence => true
+  
   
   calculated_adjustments
   
