a81b8c56bca56ae8771c365f2705364350eafab5
geermc4 <german@3drobotics.com>
2012-12-05 11:53:15 -0800

    fixing bug that gave a wrong Adjustment total
    removed some white space as well

--- app/controllers/application_controller_decorator.rb
+++ app/controllers/application_controller_decorator.rb
@@ -1,6 +1,6 @@
 ApplicationController.class_eval do
   before_filter :set_current_user
-  
+
   def set_current_user
     Spree::User.current = current_user
   end
--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,10 +1,10 @@
-Spree::OrdersController.class_eval do 
-  after_filter :create_user_group_adjustment, :only => :populate
+Spree::OrdersController.class_eval do
+  after_filter :create_user_group_adjustment, :only => [:populate, :update]
 
   def create_user_group_adjustment
     return if Spree::User.current.nil? || Spree::User.current.user_group.nil?
 
-    @adj = Spree::Adjustment.first(:conditions => [ "label = ? and source_id = ? and source_type = ? and adjustable_id = ? and adjustable_type = ?", 
+    @adj = Spree::Adjustment.first(:conditions => [ "label = ? and source_id = ? and source_type = ? and adjustable_id = ? and adjustable_type = ?",
                                                        "#{t(:user_group_adjustment)}",
                                                        Spree::User.current.user_group.id,
                                                        "Spree::UserGroup",
@@ -16,14 +16,14 @@ Spree::OrdersController.class_eval do
       @adj.source = Spree::User.current.user_group
       @adj.adjustable = @order
     end
-    
-    total_for_user = 0
+
+    user_discount = 0
 
     @order.line_items.each do |l|
-      total_for_user = (l.variant.price_for_user(Spree::User.current) * l.quantity)
+      user_discount += ((l.variant.price - l.variant.price_for_user(Spree::User.current)) * l.quantity)
     end
-       
-    @adj.amount = (total_for_user - @order.item_total) || 0
+
+    @adj.amount = (-user_discount) || 0
     @adj.save
   end
 end
