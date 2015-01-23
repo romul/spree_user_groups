339bd48d8f8b432e2ab79c1fb623b7cace9201a9
Calvin Yu <calvin@scoutmob.com>
2015-01-23 12:34:30 -0500

    Fix issue w/ broken new product action
    Tries to calculate price when newing a Product for the product form

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,12 +10,15 @@ Spree::Variant.class_eval do
   end
 
   def price
+    return 0 if new_record?
+
     if Spree::User.current.nil? or Spree::User.current.user_group.nil? then
       return prices.first.amount.to_f unless prices.first.blank? || prices.first.amount.blank?
-      return product.prices.first.amount.to_f unless product.blank? || product.prices.first.blank?
+      return product.prices.first.amount.to_f unless product.blank? || product.prices.any?
       return 0
     end
-    Spree::UserGroupsVariant.where(:user_group_id => Spree::User.current.user_group.id, :variant_id => self.id).try(:first).try(:price) || prices.first.amount.to_f
+
+    Spree::User.current.user_group.price_for_variant(self) || prices.first.amount.to_f
   end
 
   #TODO-Proper fix for this hack :)
