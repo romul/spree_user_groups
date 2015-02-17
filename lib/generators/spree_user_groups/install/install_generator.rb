45d0b4096cf1b200c5ca46c96c2e11c8c99f0e65
Roman Smirnov <POMAHC@gmail.com>
2015-02-17 12:29:08 +0300

    Don't add blank assets to a main project

--- lib/generators/spree_user_groups/install/install_generator.rb
+++ lib/generators/spree_user_groups/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeUserGroups
 			class_option :auto_run_migrations, :type => :boolean, :default => false
 
 			def add_javascripts
-				append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_user_groups\n"
-				append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_user_groups\n"
+				#append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_user_groups\n"
+				#append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_user_groups\n"
 			end
 
 			def add_stylesheets
-				inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_user_groups\n", :before => /\*\//, :verbose => true
-				inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_user_groups\n", :before => /\*\//, :verbose => true
+				#inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_user_groups\n", :before => /\*\//, :verbose => true
+				#inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_user_groups\n", :before => /\*\//, :verbose => true
 			end
 
 			def add_migrations
