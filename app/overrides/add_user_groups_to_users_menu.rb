0e79eca0945c5114f4ccfb512819e32d4c7dd3b5
hmk <hmk@users.noreply.github.com>
2015-11-09 13:25:20 -0800

    add 'original' reference for spree

--- app/overrides/add_user_groups_to_users_menu.rb
+++ app/overrides/add_user_groups_to_users_menu.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/admin/shared/_main_menu",
                      :name          => "add_user_groups_to_users_menu",
                      :replace       => "erb[loud]:contains('tab *Spree::BackendConfiguration::USER_TABS')",
-                     :erb          => "<%= main_menu_tree Spree.t(:users), icon: 'user', sub_menu: 'user', url: '#sidebar-users'")
+                     :erb          => "<%= main_menu_tree Spree.t(:users), icon: 'user', sub_menu: 'user', url: '#sidebar-users'",
+                     :original     => "a020726e074f41b4214fe71cbaa3f62385eba146")
